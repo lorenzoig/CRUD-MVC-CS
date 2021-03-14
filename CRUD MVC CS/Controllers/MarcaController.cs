94efa7190afdf92c8d2ae0a24b4ace09c2aa4eaa
Lorenzo <lorenzoibarragarcia@gmail.com>
2021-03-13 22:15:01 -0700

    Primer commit del proyecto creado

---
+++ CRUD MVC CS/Controllers/MarcaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_MVC_CS.Models;
+
+namespace CRUD_MVC_CS.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            using (var bd=new BDPasajeEntities())
+            {
+
+            }
+            return View();
+        }
+    }
+}
